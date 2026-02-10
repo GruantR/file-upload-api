4c604fd15689a709e933924978a450989972df3f
Ruslan_Trafimovich <rusworv@gmail.com>
2026-02-10 22:59:07 +0300

    Завершили написание конструктора multer

---
+++ src/config/multer.js
@@ -0,0 +1,59 @@
+const multer = require("multer");
+const path = require("path");
+const UPLOAD_DIR = "./uploads";
+
+const allowedTypes = {
+  "image/jpeg": [".jpg", ".jpeg"],
+  "image/png": [".png"],
+  "image/gif": [".gif"],
+  "application/pdf": [".pdf"],
+};
+
+const storage = multer.diskStorage({
+  destination: (req, file, callback) => {
+    callback(null, UPLOAD_DIR);
+  },
+  filename: (req, file, callback) => {
+    const extension = path.extname(file.originalname).toLowerCase();
+    const basename = path.basename(file.originalname, extension);
+    const cleanedName =
+      basename
+        .replace(/\s+/g, "-") // пробелы → дефисы
+        .replace(/[^a-zA-Z0-9-_]/g, "") // убрать спецсимволы
+        .replace(/-+/g, "-") // несколько дефисов → один
+        .substring(0, 50) || "file";
+
+    const timestamp = Date.now();
+    const random = Math.round(Math.random() * 1e9);
+
+    const newFileName = `${timestamp}-${random}-${cleanedName}${extension}`;
+    callback(null, newFileName);
+  },
+});
+
+const fileFilter = (req, file, callback) => {
+  const extension = path.extname(file.originalname).toLowerCase();
+  const mimeType = file.mimetype.split(";")[0];
+  if (
+    !extension ||
+    !mimeType ||
+    !allowedTypes[mimeType] ||
+    !allowedTypes[mimeType].includes(extension)
+  ) {
+    return callback(new Error("Недопустимый тип файла"), false);
+  }
+  return callback(null, true);
+};
+
+const limits = {
+  fileSize: 5 * 1024 * 1024,
+};
+
+const upload = multer({
+  storage: storage,
+  fileFilter: fileFilter,
+  limits: limits,
+});
+
+//multer() - Это конструктор, который создаёт middleware на основе твоих настроек. Без вызова multer() — это просто объект.
+module.exports = upload;
--- src/server.js
+++ src/server.js
@@ -1,31 +1,47 @@
 //src/server.js
 // только запуск сервера
 require("dotenv").config();
-const {initializeDatabase} = require('./models/index');
+const { initializeDatabase } = require("./models/index");
 const app = require("./app");
+
+const fs = require("fs").promises;
+
 const PORT = process.env.PORT;
+const UPLOADS_DIR = "./uploads";
+
+async function ensureUploadsFolder() {
+  try {
+    await fs.mkdir(UPLOADS_DIR, { recursive: true });
+    console.log(`Папка ${UPLOADS_DIR} успешно сохдана`);
+  } catch (err) {
+    if (err.code === "EEXIST") {
+      console.log(`Папка ${UPLOADS_DIR} уже существует`);
+    } else {
+      console.error("Ошибка создания папки:", err.message);
+      throw err;
+    }
+  }
+}
 
 async function startServer() {
-  try{
+  try {
     const dbConnected = await initializeDatabase();
-       if (!dbConnected) {
-      throw new Error('Не удалось подключиться к БД');
+    if (!dbConnected) {
+      throw new Error("Не удалось подключиться к БД");
     }
+
     app.listen(PORT, () => {
-  console.log(`Сервер запущен на порту ${PORT}`);
-  console.log(`📁 База данных: ${process.env.DB_NAME}`);
-});
-
+      console.log(`Сервер запущен на порту ${PORT}`);
+      console.log(`📁 База данных: ${process.env.DB_NAME}`);
+    });
+  } catch (err) {
+    console.error("❌ Ошибка запуска:", err);
+    console.error("   Проверь:");
+    console.error("   1. Запущен ли PostgreSQL?");
+    console.error("   2. Правильные ли логин/пароль в .env?");
+    console.error("   3. Существует ли БД", process.env.DB_NAME, "?");
+    process.exit(1); //  команда для немедленного завершения процесса Node.js с указанием кода выхода (1 — НЕУДАЧА (ошибка, сбой))
   }
-  catch(err){
-  console.error('❌ Ошибка запуска:', err);
-  console.error('   Проверь:');
-  console.error('   1. Запущен ли PostgreSQL?');
-  console.error('   2. Правильные ли логин/пароль в .env?');
-  console.error('   3. Существует ли БД', process.env.DB_NAME, '?');
-  process.exit(1)//  команда для немедленного завершения процесса Node.js с указанием кода выхода (1 — НЕУДАЧА (ошибка, сбой))
-  }
-};
-
+}
+ensureUploadsFolder();
 startServer();
-
