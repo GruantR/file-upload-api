0fe6c14900c90d4396e4d4f411fc7730edb615e2
Ruslan_Trafimovich <rusworv@gmail.com>
2026-03-20 21:39:27 +0300

    test

--- src/routes/uploadRoutes.js
+++ src/routes/uploadRoutes.js
@@ -9,8 +9,8 @@ const validate = require('../middleware/validateExpress');
 
 router.post("/", authMiddleware, upload.single("file"), fileValidator.fileRequiredValidation(), fileValidator.uploadFileValidation(), validate, uploadController.uploadFile);
 router.get("/:uuid", authMiddleware, fileValidator.uuidParamValidation(), validate, uploadController.getFile.bind(uploadController));
-router.get("/:uuid/download", authMiddleware, fileValidator.uuidParamValidation(), validate, uploadController.forceDownloadFile.bind(uploadController));
 router.get("/", authMiddleware, fileValidator.paginationValidation(), validate, uploadController.getAllFiles);
 router.delete("/:uuid", authMiddleware, fileValidator.uuidParamValidation(), validate, uploadController.deleteFile.bind(uploadController));
+
 
 module.exports = router;
